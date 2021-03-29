c7769d7fa53264f93079b4a1c94ff5ebf4891d09
fordpic <fordpickert@gmail.com>
2021-03-29 12:22:03 -0500

    finish backend deployment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello from nodemon');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+	console.log(`app listening on port ${port}`);
+});
